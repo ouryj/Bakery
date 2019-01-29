ded09184155d83a5a025351058ecaa240f0878b8
mohamed jalloh <m.ouryjalloh@yahoo.com>
2019-01-29 00:29:50 -0500

    just added some style to it

--- public/main.js
+++ public/main.js
@@ -1,4 +1,6 @@
 let s1 = document.querySelector("#send");
-s1.addEventLListener("click", (event)=>{
-    alert("we have received your request check your inbox for more details")
+s1.addEventListener("click", (event)=>{
+    event.preventDefault();
+
+    alert("we have received your request check your inbox for more details");
 });
